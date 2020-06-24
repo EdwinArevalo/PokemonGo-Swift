39b72edf3ed65a03afb69264b9d13f199780ec5a
EdwinArevalo <edwin.arevalo@tecsup.edu.pe>
2020-06-24 02:27:12 -0500

    ubication updated

--- PokemonGo/ViewController.swift
+++ PokemonGo/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var ubicacion = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ubicacion.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            mapView.showsUserLocation = true
+            ubicacion.startUpdatingLocation()
+        }else {
+            ubicacion.requestWhenInUseAuthorization()
+        }
+        
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Ubicacion actualizada")
     }
 
 
