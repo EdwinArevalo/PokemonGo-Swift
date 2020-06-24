5f58b04e3f7cfd5af8c57b91a011bdda72aa1066
EdwinArevalo <edwin.arevalo@tecsup.edu.pe>
2020-06-24 01:41:57 -0500

    first commit

---
+++ PokemonGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonGo
+//
+//  Created by mbtec22 on 6/24/20.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
